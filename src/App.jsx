21ad548da261058b0f63494158ed26fb9c00e1e5
tom dav <kobe242tomy@icloud.com>
2023-06-11 19:48:26 +0200

    level1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header className="Header">
+        <h1>
+          <img
+            src="https://i0.wp.com/www.pokemon-france.com/wp-content/uploads/2013/02/globallink.png?resize=872%2C480&ssl=1"
+            alt="logo pokecenter"
+          />
+        </h1>
+        <img
+          src="https://www.pwo-wiki.info/images/c/c2/Pokemon_Center_Exterior.png"
+          alt="logo pokecenter"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
